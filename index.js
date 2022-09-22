07c827228cb1ed65a447131a2e609f6b2e032877
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-22 11:08:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+const { prefix, token } = require("./config.json");
 
 client.on("ready", () => {
   console.log("Bot online!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("!oi")) {
-    var msg = message.content.split(" ");
-    message.reply(msg);
-  }
+  if (message.author.bot) return;
+
+  let mentions = message.mentions.users.map((item) => {
+    if (message.content.startsWith(`${prefix}avise`)) var userID = item.id;
+    userID = client.users.cache.get(userID);
+    console.log(userID);
+    var m = message.content.split("<");
+    m = m[m.length - 1].replace(">", "");
+
+    userID.send(
+      `@${message.author.username} mencionou você com a mensagem: ${m}`
+    );
+  });
 });
 
-client.login(config.token);
+client.login(token);
