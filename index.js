3dc9ea343370032b9a16704f85e51f4fb36befc8
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-23 10:31:00 -0300

    .

--- index.js
+++ index.js
@@ -2,21 +2,41 @@ const Discord = require("discord.js");
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
+const axios = require("axios");
 const { prefix, token } = require("./config.json");
 
 client.on("ready", () => {
   console.log("Bot online!");
 });
 
-client.on("messageReactionAdd", async (reaction, user) => {
-  if (reaction.message.id === "1022848661190889492") {
-    const guildMember = reaction.message.guild.members.cache.get(user.id);
-    if (!guildMember.roles.cache.get("1022849753509265478")) {
-      guildMember.roles.add("1022849753509265478");
-    } else {
-      guildMember.roles.remove("1022849753509265478");
-    }
-  }
+client.on("message", async (message) => {
+  serverInfo.init(message);
 });
+
+const serverInfo = {
+  data: null,
+  message: null,
+  commands: function () {
+    if (this.message.content.startsWith("!status")) {
+      this.message.channel.send(`O servidor ${this.data.hostname} está, atualmente, com ${this.data.players.online} players online!`);
+    }
+  },
+
+  init: function (message) {
+    this.message = message;
+
+    const data = axios({
+      url: "https://api.mcsrvstat.us/1/mc.hypixel.net:25565",
+      headers: { 'accept': "application/json" },
+    })
+      .then((result) => {
+        this.data = result.data;
+        this.commands();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
 
 client.login(token);
