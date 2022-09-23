73ea563c111659200ee51203314aff606e9cd107
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-23 09:47:19 -0300

    .

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
 const { prefix, token } = require("./config.json");
 
 client.on("ready", () => {
   console.log("Bot online!");
 });
 
-client.on("message", (message) => {
-  if (message.author.bot) return;
-
-  let mentions = message.mentions.users.map((item) => {
-    if (message.content.startsWith(`${prefix}avise`)) var userID = item.id;
-    userID = client.users.cache.get(userID);
-    console.log(userID);
-    var m = message.content.split("<");
-    m = m[m.length - 1].replace(">", "");
-
-    userID.send(
-      `@${message.author.username} mencionou você com a mensagem: ${m}`
-    );
-  });
+client.on("messageReactionAdd", async (reaction, user) => {
+  if (reaction.message.id === "1022848661190889492") {
+    const guildMember = reaction.message.guild.members.cache.get(user.id);
+    if (!guildMember.roles.cache.get("1022849753509265478")) {
+      guildMember.roles.add("1022849753509265478");
+    } else {
+      guildMember.roles.remove("1022849753509265478");
+    }
+  }
 });
 
 client.login(token);
